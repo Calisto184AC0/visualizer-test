f48752d432f8e7750bfcc1f16aabbeff3aeb95ae
Arturo <arturorjach@gmail.com>
2023-03-29 12:22:07 +0200

    💄 Position fixed to screen

--- src/components/Composition/components/Alert.jsx
+++ src/components/Composition/components/Alert.jsx
@@ -1,6 +1,6 @@
 const Alert = ({ message }) => {
   return (
-    <span className='absolute top-6 left-1/2 transform -translate-x-1/2 px-3 py-1 bg-black/[.5] text-white rounded-md'>
+    <span className='fixed z-20 top-6 left-1/2 transform -translate-x-1/2 px-3 py-1 bg-black/[.5] text-white rounded-md'>
       {message}
     </span>
   )
