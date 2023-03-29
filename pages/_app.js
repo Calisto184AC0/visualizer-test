210c7b835e2f1fe005601da9fff9e79f5e0786d6
Arturo <arturorjach@gmail.com>
2023-03-29 20:54:07 +0200

    🐛 Fix visual bug on mobile devices

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,13 @@
 import '../styles/globals.css'
 import { PartsProvider } from '../src/context/PartsContext'
+import Head from 'next/head'
 
 function MyApp ({ Component, pageProps }) {
   return (
     <PartsProvider>
+      <Head>
+        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+      </Head>
       <Component {...pageProps} />
     </PartsProvider>
   )
