02899f7d63829eab32ee71af3474d38617a6f1f8
Arturo <arturorjach@gmail.com>
2023-03-29 15:45:09 +0200

    💄 Change background color

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import MaterialMenu from '../src/components/MaterialMenu'
 
 const Home = () => {
   return (
-    <div className='relative flex flex-col items-center justify-center min-h-screen'>
+    <div className='relative flex flex-col items-center justify-center min-h-screen bg-stone-100'>
       <MaterialMenu />
       <Composition />
     </div>
